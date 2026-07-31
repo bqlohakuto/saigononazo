5c7feb5c6fbb1f396c7a9fbd5158bd7a68ab394a
bqlohakuto <nzkj6d7pj8@privaterelay.appleid.com>
2026-08-01 02:32:47 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,5 +1,74 @@
-const startButton = document.getElementById("startButton");
+//==============================
+// ゲーム画面
+//==============================
 
-startButton.addEventListener("click", () => {
-    alert("オープニングへ");
-});
+const game = document.getElementById("game");
+
+
+//==============================
+// タイトル画面
+//==============================
+
+function showTitle(){
+
+    game.innerHTML = `
+
+    <div class="title-screen">
+
+        <h1>最後の謎が解けるまで</h1>
+
+        <button id="startButton">
+
+            はじめる
+
+        </button>
+
+    </div>
+
+    `;
+
+    document
+        .getElementById("startButton")
+        .addEventListener("click", showNameInput);
+
+}
+
+
+//==============================
+// 名前入力画面
+//==============================
+
+function showNameInput(){
+
+    game.innerHTML = `
+
+    <div class="title-screen">
+
+        <h2>あなたの名前を入力してください</h2>
+
+        <input
+            id="playerName"
+            type="text"
+            maxlength="8"
+            placeholder="主人公の名前">
+
+        <br><br>
+
+        <button id="decideButton">
+
+            決定
+
+        </button>
+
+    </div>
+
+    `;
+
+}
+
+
+//==============================
+// ゲーム開始
+//==============================
+
+showTitle();

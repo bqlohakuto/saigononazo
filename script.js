18262e878cba958e15065a95aceb526675d16783
bqlohakuto <nzkj6d7pj8@privaterelay.appleid.com>
2026-08-01 03:29:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,11 +18,11 @@ let playerName = "";
 // 演出時間
 //==================================
 
-const FLASH_TIME = 300;
+const FLASH_TIME = 1000;
 
-const BLACK_TIME = 2000;
+const BLACK_TIME = 3000;
 
-const FADE_TIME = 3000;
+const FADE_TIME = 6000;
 
 
 //==================================
